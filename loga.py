c1c3b52d3781b4f9c9622f556ed9c3b929854cbd
Frisky471 <maksimluzin47@gmail.com>
2025-12-12 21:35:13 +0300

    добавил фамилию

--- loga.py
+++ loga.py
@@ -3,8 +3,9 @@ logo2 = "S____ L      T"
 logo3 = "SSSSS L      T"
 logo4 = "____S L      T"
 logo5 = "SSSSS LLLLL  T"
-name = "Максим"
+name = "Максим Лузин"
 age = "16"
 
 print(logo1, logo2, logo3, logo4, logo5, sep = '\n')
 print("Привет, " + name + ". Тебе " + age + " лет.")
+
