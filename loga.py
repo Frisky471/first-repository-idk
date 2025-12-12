092dd3ae03bbbc9041755e68b973313decf664b1
Frisky471 <maksimluzin47@gmail.com>
2025-12-12 20:40:42 +0300

    Add files via upload
    my logo, name and age

---
+++ loga.py
@@ -0,0 +1,10 @@
+logo1 = "SSSSS L    TTTTT"
+logo2 = "S____ L      T"
+logo3 = "SSSSS L      T"
+logo4 = "____S L      T"
+logo5 = "SSSSS LLLLL  T"
+name = "Максим"
+age = "16"
+
+print(logo1, logo2, logo3, logo4, logo5, sep = '\n')
+print("Привет, " + name + ". Тебе " + age + " лет.")
